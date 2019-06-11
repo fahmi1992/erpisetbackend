efc8ca0339180a2ed79df096ade844082c66d67e
fahmi1992 <fahmi.aouissaoui1@gmail.com>
2019-06-11 13:54:47 +0100

    first commit

---
+++ services/src/main/java/tn/iset/SecurityDbExampleApplication.java
@@ -0,0 +1,28 @@
+package tn.iset;
+
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import tn.iset.controller.StorageService;
+
+
+
+
+@SpringBootApplication
+
+public class SecurityDbExampleApplication implements CommandLineRunner {
+	@Resource
+	StorageService storageService;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityDbExampleApplication.class, args);
+	}
+	@Override
+	public void run(String... arg) throws Exception {
+	
+	}
+}
